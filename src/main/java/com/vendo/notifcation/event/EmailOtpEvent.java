37060a1fd2787df6059801419f74a4f4f8c1c909
vlcdck <pridatchenkovladislav@gmail.com>
2026-02-16 16:20:41 +0200

    Init

---
+++ src/main/java/com/vendo/notifcation/event/EmailOtpEvent.java
@@ -0,0 +1,27 @@
+package com.vendo.notifcation.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailOtpEvent {
+
+    private String otp;
+
+    private String email;
+
+    private OtpEventType otpEventType;
+
+    public enum OtpEventType {
+
+        EMAIL_VERIFICATION,
+
+        PASSWORD_RECOVERY
+
+    }
+}
