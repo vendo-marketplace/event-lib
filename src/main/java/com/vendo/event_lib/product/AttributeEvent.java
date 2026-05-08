ca1d7e260fd2bdeb0cc808f626d7809a001b7863
HaaaamBurger <gemasclashes@gmail.com>
2026-05-08 20:23:23 +0300

    added id back to Attribute

--- src/main/java/com/vendo/event_lib/product/AttributeEvent.java
+++ src/main/java/com/vendo/event_lib/product/AttributeEvent.java
@@ -3,6 +3,7 @@ package com.vendo.event_lib.product;
 import java.util.List;
 
 public record AttributeEvent(
+        String id,
         String title,
         String type,
         List<String> values
