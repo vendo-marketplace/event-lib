fe88b13f4af3653719fc5501593cddaf390845de
HaaaamBurger <gemasclashes@gmail.com>
2026-08-25 16:57:26 +0300

    kan-112: AutoSearchMatchingEvent

--- src/main/java/com/vendo/event_lib/auto_search/AutoSearchEmailEvent.java
+++ src/main/java/com/vendo/event_lib/auto_search/AutoSearchEmailEvent.java
@@ -1,4 +1,9 @@
 package com.vendo.event_lib.auto_search;
 
 public record AutoSearchEmailEvent(String id, String email) {
+
+    public static AutoSearchEmailEvent from(String id, String email) {
+        return new AutoSearchEmailEvent(id, email);
+    }
+
 }
