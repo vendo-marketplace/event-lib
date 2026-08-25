d71549b5e79a8c069dd8c82071614db4c8ecb98d
HaaaamBurger <gemasclashes@gmail.com>
2026-08-25 08:40:58 +0300

    kan-112: removed products from auto search

--- src/main/java/com/vendo/event_lib/auto_search/AutoSearchEmailEvent.java
+++ src/main/java/com/vendo/event_lib/auto_search/AutoSearchEmailEvent.java
@@ -1,18 +1,4 @@
 package com.vendo.event_lib.auto_search;
 
-import java.math.BigDecimal;
-import java.util.List;
-
-public record AutoSearchEmailEvent(
-        String id,
-        String email,
-        List<ResultProduct> products
-) {
-    public record ResultProduct(
-            String id,
-            String title,
-            BigDecimal price
-    ) {
-    }
-
+public record AutoSearchEmailEvent(String id, String email) {
 }
