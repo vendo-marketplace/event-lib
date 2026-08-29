a6391be985901609b38985aeb56cd0525d47bb31
HaaaamBurger <gemasclashes@gmail.com>
2026-08-29 14:18:58 +0300

    kan-112: added email field to AutoSearchMatchingEvent

--- src/main/java/com/vendo/event_lib/auto_search/AutoSearchMatchingEvent.java
+++ src/main/java/com/vendo/event_lib/auto_search/AutoSearchMatchingEvent.java
@@ -1,9 +1,9 @@
 package com.vendo.event_lib.auto_search;
 
-public record AutoSearchMatchingEvent(String id) {
+public record AutoSearchMatchingEvent(String id, String email) {
 
-    public static AutoSearchMatchingEvent from(String id) {
-        return new AutoSearchMatchingEvent(id);
+    public static AutoSearchMatchingEvent from(String id, String email) {
+        return new AutoSearchMatchingEvent(id, email);
     }
 
 }
